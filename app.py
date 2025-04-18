e33e0982a16fad8745ce8c51b6be2230a8ef89e1
journalfederal <sefa.cintika@gmail.com>
2025-04-18 04:19:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-
-
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.', static_folder='.')
 
 @app.route('/')
 def home():
-    return "Hello from Journal Federal!"
+    return render_template('journal-federal.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
