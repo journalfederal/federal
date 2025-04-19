5c8e474a62bf7051c4da94527d6d050594fabc23
journalfederal <sefa.cintika@gmail.com>
2025-04-19 18:25:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-<file name=0 path=/Users/sefaer/Desktop/feder/bekend/video_kes.py>cmd = [
+from flask import Flask, render_template, request, redirect, url_for, render_template_string
+
+cmd = [
     "yt-dlp",
     "-f", "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4",
     "-o", downloaded_path,
@@ -7,4 +9,3 @@
 
 result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 print("FFmpeg çıktı:", result.stderr)
-</file>
